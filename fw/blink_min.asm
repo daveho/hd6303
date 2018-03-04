c9c8e9a7c176e35d0454b2872cc0829c0a93aebc
David Hovemeyer <david.hovemeyer@gmail.com>
2018-03-03 20:08:11 -0500

    blinking LEDs is working!
    Rate is too fast: need to improve the delay loop.

--- fw/blink_min.asm
+++ fw/blink_min.asm
@@ -9,18 +9,29 @@ PORT2_OUT equ $0003
 	org $E000
 
 main subroutine
-	;; Locate stack at top of internal RAM
-	lds #$00FF
+	;; Locate stack in middle of internal RAM
+	lds #$00C0
 
 	;; Configure pins P23 and P24 for output
 	ldaa #$18
 	staa PORT2_DDR
 
+.1
+	;; Turn on LED on P23
+	ldaa #$08
+	staa PORT2_OUT
+	jsr delay
+
 	;; Turn on LED on P24
 	ldaa #$10
 	staa PORT2_OUT
+	jsr delay
 
-	;; Just loop infinitely
+	jmp .1
+
+
+delay subroutine
+	ldab #$00
 .1
 	nop
 	nop
@@ -28,7 +39,68 @@ main subroutine
 	nop
 	nop
 	nop
-	jmp .1
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	incb
+	cmpb #$ff
+	bne .1
+	rts
 
 ;; PC for reset vector
 	org $FFFE
